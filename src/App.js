d3d9feb428dc5eefc616c1d212600dadaa0fcf20
mariagoldamg <mariamelnikova88@gmail.com>
2023-07-20 15:46:44 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+
+import { useMemo, useState } from 'react';
 import './App.css';
 
+const fruits = [
+  {id: '1', item: 'Peach'},
+    {id: '2', item: 'Apple'},
+      {id: '3', item: 'Banana'},
+        {id: '4', item: 'Pineapple'},
+          {id: '5', item: 'Plum'},
+]
+
 function App() {
+
+  const [text, setText] = useState('');
+  const [search, setSearch] = useState('')
+
+ const handleText=(e)=>{
+setText(e.target.value);
+console.log(e.target.value)
+ }
+ const handleClick=()=>{
+setSearch(text)
+ }
+
+ //If we keep the logic this way, 
+ //all the elements are rendering with every letter typed(not the word)
+ //so we will rewrite this piece of code with useMemo
+
+//useMemo is used to optimize the productivity of our app
+
+/* const filteredFruits = fruits.filter((fruit)=>{
+  return fruit.item.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+ })*/
+
+
+ const filteredFruits = useMemo (()=> fruits.filter((fruit)=>{
+  console.log('FILTERING!!!')
+  return fruit.item.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+ }), [search])//put dependency here
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Filtering</h1>
+     <input type='text' onChange={handleText}></input>
+     <button type='button' onClick={handleClick}>Search</button>
+     <div>
+      {filteredFruits.map((filteredFruit)=>(
+ <p key={filteredFruit.id}>{filteredFruit.item}</p>
+      ))}
+     
+     </div>
     </div>
   );
 }
